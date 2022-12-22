782529d58f0cce893af54693b088166b27eb5977
Konstantin Konstantin <zetslief@protonmail.com>
2022-12-22 20:52:38 +0100

    Draw arena.

--- ui/main.ts
+++ ui/main.ts
@@ -2,9 +2,36 @@ const BLACK = "black";
 const BACKGROUND = "#3333dd"
 const PLAYER = "#ff3333"
 
+// MATHTYPE
+
+interface Vec2 { x: number, y: number }
+type Point = Vec2;
+
+function vec2(x: number, y: number): Vec2 {
+    return {x, y};
+}
+
+function distance(from: Point, to: Point): number {
+    const dx = to.x - from.x;
+    const dy = to.x - from.x;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+function direction(from: Point, to: Point): Vec2 {
+    const dst = Math.sqrt(distance(from, to));
+    if (dst < 0.001) {
+        console.warn("Small distance", dst);
+        return vec2(0, 0);
+    }
+    return vec2(
+        (to.x - from.x) / dst,
+        (to.y - from.y) / dst
+    );
+}
+
 type GameState = {
-    loopInterval: number,
     player: Player,
+    arena: Arena,
     spell: Spell | null,
     inputState: InputState
 }
@@ -32,18 +59,21 @@ type Spell = {
 }
 
 type InputState = {
-    cast: Cast | null
+    cast: Cast | null,
+    player: Vec2
 }
 type InputUpdate = {
     cast: Cast | null
+    player: Vec2
 }
 
 function setupState(): GameState {
+    const arena = { x: window.innerWidth / 2, y: window.innerHeight / 2, radius: 500 };
     return {
-        loopInterval: 100,
-        player: { x: 40, y: 40, size: 20, chakra: { timeout: 0 } },
+        player: { x: arena.x, y: arena.y, size: 20, chakra: { timeout: 0 } },
+        arena,
         spell: null,
-        inputState: { cast: null },
+        inputState: { cast: null, player: vec2(0, 0) },
     };
 }
 
@@ -51,10 +81,27 @@ function setupHandlers(inputState: InputState) {
     document.addEventListener("click", (e) => {
         inputState.cast = { x: e.pageX, y: e.pageY };
     });
+    document.addEventListener("keydown", (e) => {
+        if (e.isComposing || e.keyCode === 229) {
+            return;
+        }
+        const key = e.code.toUpperCase();
+        if (key === "W") {
+            inputState.player.y = 1;
+        }
+        if (key === "S") {
+            inputState.player.y = -1;
+        }
+        if (key === "D") {
+            inputState.player.x = 1;
+        }
+        if (key === "A") {
+            inputState.player.x = -1;
+        }
+    });
 }
 
 // DRAWING
-
 function drawRect(
     ctx: CanvasRenderingContext2D,
     x: number,
@@ -91,13 +138,28 @@ function drawSpell(
 ) {
     ctx.fillStyle = "green";
     ctx.beginPath();
-    ctx.arc(spell.x1, spell.y1, 20 / 2, 0, 2 * Math.PI);
+    ctx.arc(spell.x0, spell.y0, 20 / 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+type Arena = {
+    x: number,
+    y: number,
+    radius: number
+}
+function drawArena(
+    ctx: CanvasRenderingContext2D,
+    arena: Arena
+) {
+    ctx.fillStyle = "orange";
+    ctx.beginPath();
+    ctx.arc(arena.x, arena.y, arena.radius, 0, 2 * Math.PI);
     ctx.fill();
 }
 
 // PROCESSING
 function processInput(inputState: InputState): InputUpdate {
-    let input: InputUpdate = { cast: null };
+    let input: InputUpdate = { cast: null, player: vec2(0, 0) };
     if (inputState.cast) {
         input.cast = inputState.cast;
         inputState.cast = null;
@@ -107,33 +169,52 @@ function processInput(inputState: InputState): InputUpdate {
 
 function applyInput(state: GameState, inputChange: InputUpdate) {
     function spell(x0: number, y0: number, x1: number, y1: number): Spell {
-        return { x0: x0, y0: y0, x1: x1, y1: y1, speed: 10 };
+        const defaultSpeed = 10;
+        return { x0: x0, y0: y0, x1: x1, y1: y1, speed: defaultSpeed };
     }
     if (inputChange.cast) {
         state.spell = spell(state.player.x, state.player.y, inputChange.cast.x, inputChange.cast.y);
         console.log(state.spell);
     }
+    const playerMoveLength = distance(vec2(0, 0), inputChange.player);
+    if (playerMoveLength > 0) {
+    }
 }
 
-function updatePhysics(_state: GameState) {
+function updatePhysics(state: GameState, dt: number) {
+    function spellDistance(spell: Spell): number {
+        return distance(vec2(spell.x0, spell.y0), vec2(spell.x1, spell.y1));
+    }
+    if (state.spell) {
+        const spell = state.spell
+        const dist = spellDistance(spell);
+        const spellExitDistance = 10;
+        if (dist < spellExitDistance) {
+            state.spell = null;
+        } else {
+            const dir = direction(vec2(spell.x0, spell.y0), vec2(spell.x1, spell.y1));
+            spell.x0 = spell.x0 + dir.x * spell.speed * dt;
+            spell.y0 = spell.y0 + dir.y * spell.speed * dt;
+        }
+    }
 }
 
 // MAIN
-
 function draw(state: GameState, render: RenderState) {
     const canvas = render.canvas;
     const ctx = render.ctx;
     drawBackground(ctx, canvas.width, canvas.height);
+    drawArena(ctx, state.arena);
     drawPlayer(ctx, state.player)
     if (state.spell) {
         drawSpell(ctx, state.spell);
     }
 }
 
-function loop(game: GameState, render: RenderState) {
+function loop(game: GameState, render: RenderState, deltaTime: number) {
     const update = processInput(game.inputState);
     applyInput(game, update);
-    updatePhysics(game);
+    updatePhysics(game, deltaTime);
     draw(game, render)
 }
 
@@ -148,8 +229,9 @@ function setupRenderState(): RenderState {
 function main() {
     const state = setupState();
     const renderState = setupRenderState();
+    const deltaTime = 1000 / 60;
     setupHandlers(state.inputState)
-    setInterval(() => loop(state, renderState), state.loopInterval);
+    setInterval(() => loop(state, renderState, deltaTime * 0.001), deltaTime);
 }
 
 main()
