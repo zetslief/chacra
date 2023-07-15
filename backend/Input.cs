972d3a318ac72db9aa731397802d65682c78e227
Konstantin Konstantin <zetslief@protonmail.com>
2023-07-15 19:04:20 +0200

    Staring BE from scratch.

--- backend/Input.cs
+++
@@ -1,2 +0,0 @@
-public record AbilityToggle(string Type);
-public record Click(float X, float Y) : Point(X, Y); 
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,24 +1,6 @@
 using Microsoft.Extensions.FileProviders;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDbContext<Database>();
-builder.Services.AddSingleton<GameStateService>();
-builder.Services.AddHostedService<GameLoopService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(configuration =>
-{
-     configuration.SwaggerDoc(
-        "v1",
-         new OpenApiInfo 
-         { 
-            Title = "Chacra API",
-            Description = "API for Chacra",
-            Version = "v1" 
-        });
-});
 
 var app = builder.Build();
 
@@ -33,25 +15,8 @@ app.UseStaticFiles(new StaticFileOptions
         Path.Combine(builder.Environment.ContentRootPath, "../ui/dist/"))
 });
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Chacra API v1");
-});
-
-app.MapGet("/", (GameStateService serivce) => {
-    return serivce.GetState()
-        .Enemies
-        .ToArray();
-});
-
-app.MapPost("/killEnemy/{enemy}", (Enemy enemy, GameStateService service) => 
-{
-    //service.QueueKilledEnemy(enemy);
-});
-
-app.MapGet("/initializeGameState", (GameStateService service) => {
-    return service.GetState();
+app.MapGet("/", () => {
+    return "true";
 });
 
 app.Run();
