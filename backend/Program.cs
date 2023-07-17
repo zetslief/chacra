cbbd29f03938c8aebf6d0691f2e4270efb04aa8a
Konstantin Konstantin <zetslief@protonmail.com>
2023-07-17 22:02:21 +0200

    Processing form data on be

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,7 +2,8 @@ using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var indexPath = Path.GetFullPath("../ui/dist/index.html");
+var loginPagePath = Path.GetFullPath("../ui/dist/login.html");
+var indexPagePath = Path.GetFullPath("../ui/dist/index.html");
 
 var app = builder.Build();
 
@@ -18,7 +19,23 @@ app.UseStaticFiles(new StaticFileOptions
 });
 
 app.MapGet("/", () => {
-    return Results.Content(File.ReadAllText(indexPath), "text/html");
+    return Results.Content(File.ReadAllText(loginPagePath), "text/html");
+});
+
+app.MapPost("/connect", async (ctx) => {
+    var form = await ctx.Request.ReadFormAsync();
+    if (form.TryGetValue("playerName", out var playerName)) {
+        Console.WriteLine($"Player name: {playerName}");
+        ctx.Response.Redirect($"/connect/{playerName}");
+    }
+});
+
+app.MapGet("/connect/{playerName}", (string playerName) => {
+    return Results.Content(playerName);
+});
+
+app.MapGet("/game", () => {
+    return Results.Content(File.ReadAllText(loginPagePath), "text/html");
 });
 
 app.Run();
