85dd2930719c97a2e5bfa02b3cbb800f0036323e
Konstantin Konstantin <zetslief@protonmail.com>
2023-07-29 22:12:29 +0200

    Bot: refactoring, moving code into separate function.

--- ui/bot.ts
+++ ui/bot.ts
@@ -1,11 +1,16 @@
 import { InputState } from './lib/types';
 
 const uri = "http://localhost:5000/game/input";
+const inputUpdateDelay = 50;
 
 Promise.all(Array.from({length: 12}, (k, v) => runBot(uri, "Player" + v)))
     .then(() => console.log("Bot finished!"));
 
 async function runBot(uri: string, playerName: string) {
+    sendInputState(uri, playerName);
+}
+
+function sendInputState(uri: string, playerName: string) {
     const state = new InputState(playerName);
     state.dy = 1;
     fetch(uri, {
@@ -13,9 +18,9 @@ async function runBot(uri: string, playerName: string) {
         body: JSON.stringify(state),
         headers: { "Content-Type": "application/json" }
     })
-        .then(() => setTimeout(() => runBot(uri, playerName), 50))
+        .then(() => setTimeout(() => runBot(uri, playerName), inputUpdateDelay))
         .catch((e) => {
             console.error(e);
-            setTimeout(() => runBot(uri, playerName), 50);
+            setTimeout(() => sendInputState(uri, playerName), inputUpdateDelay);
         });
 }
