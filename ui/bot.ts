02a63c3975f5d2854ec4355ed136a7820d4a0776
Konstantin Konstantin <zetslief@protonmail.com>
2023-08-05 19:48:40 +0200

    Bot waits for lobby to be started

--- ui/bot.ts
+++ ui/bot.ts
@@ -3,6 +3,7 @@ import { InputState } from './lib/types';
 const root = "http://localhost:5000";
 
 const connectUri = root + "/lobby/connect";
+const statusUri = root + "/lobby/status";
 const inputUpdateUri = root + "/game/input";
 
 const inputUpdateDelay = 50;
@@ -10,20 +11,38 @@ const inputUpdateDelay = 50;
 Promise.all(Array.from({length: 12}, (k, v) => runBot("Player" + v)))
 
 async function runBot(playerName: string) {
+    console.log("Connecting...", playerName);
     await sendConnect(connectUri, playerName);
+    console.log("connected!. Waiting for start...");
+    await waitForStart(statusUri);
+    console.log("Start!");
     sendInputState(inputUpdateUri, playerName);
 }
 
 async function sendConnect(uri: string, playerName: string) {
-    const body = JSON.stringify({
-        playerName
-    });
-    console.log("Connecting", body);
     await fetch(uri, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body,
+        body: JSON.stringify({
+            playerName
+        }),
     });
+}
+
+interface LobbyStatus {
+    started: boolean;
+}
+async function getLobbyStatus(uri: string): Promise<LobbyStatus> {
+    const result = await fetch(uri);
+    return await result.json() as LobbyStatus; 
+}
+
+async function waitForStart(uri: string) {
+    const status = await getLobbyStatus(uri);
+    if (!status.started) {
+        await new Promise(resolve => setTimeout(resolve, 10));
+        await waitForStart(uri);
+    }
 }
 
 function sendInputState(uri: string, playerName: string) {
