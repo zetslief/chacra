51a7111b13aba43601806d537edb8f5daff0dcb5
Konstantin Konstantin <zetslief@protonmail.com>
2023-08-05 21:01:26 +0200

    Fix login html and js

--- ui/dist/js/login.js
+++ ui/dist/js/login.js
@@ -1,7 +1,27 @@
 const input = document.getElementById("playerName");
 
-function savePlayer() {
+async function connect() {
     sessionStorage.setItem("playerName", input.value);
+    const result = await fetch(
+        "http://localhost:5000/lobby/connect",
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ playerName: input.value }),
+        }
+    );
+    if (result.ok) {
+        console.log(input.value, "is connected!");
+        if (result.redirected && result.url) {
+            location.assign(result.url);
+        } else {
+            console.error("Result is ok, but where should I go? No redirection :(");
+            console.error(result);
+        }
+    } else {
+        console.error("Failed to connect", input.value);
+        console.error(result);
+    }
 }
 
 window.onload = () => {
