444214687374f3a953b926e7b4964c26186096ec
Konstantin Konstantin <zetslief@protonmail.com>
2023-08-30 21:21:19 +0200

    A little bit more work on game state serialization. At this point it is clear that sending whole state on each frame is impossible.

--- ui/tests/game_state_serialization.ts
+++ ui/tests/game_state_serialization.ts
@@ -22,7 +22,8 @@ function addAreaBooster(
 function setup(sampleCount: number): AreaBoosters {
     const areaBoosters = createAreaBoosters();
     for (let index = 0; index < sampleCount; ++index) {
-        addAreaBooster(areaBoosters, Math.random(), Math.random(), Math.random(), Math.random(), "black");
+        const value = 1 / 3.33;
+        addAreaBooster(areaBoosters, value, value, value, value, "black");
     }
     return areaBoosters;
 }
@@ -33,16 +34,36 @@ function testJson(sampleCount: number): number {
     return result.length;
 }
 
-function runSerializationTests(): boolean {
+async function testArray(sampleCount: number): Promise<number> {
+    const ab = setup(sampleCount);
+    const x = new Float32Array(sampleCount);
+    const y = new Float32Array(sampleCount);
+    const radius = new Uint8Array(sampleCount);
+    const duration = new Uint8Array(sampleCount);
+    // TODO: use color as RGBA value in the code, not string.
+    const color = new Uint8Array(sampleCount);
+
+    const xx = new Uint8Array(x.buffer);
+    const yy = new Uint8Array(y.buffer);
+    const rradius = new Uint8Array(radius.buffer);
+    const dduration = new Uint8Array(duration.buffer);
+    const ccolor = new Uint8Array(color.buffer);
+
+    var blob = new Blob([xx, yy, rradius, dduration, ccolor], {type: "octet/stream"});
+    return (await blob.arrayBuffer()).byteLength;
+}
+
+async function runSerializationTests() {
     const sampleCount = 1000;
     const jsonResult = testJson(sampleCount);
     console.log("JSON: ", jsonResult);
-    return true;
+    const arrayResult = await testArray(sampleCount);
+    console.log("Typed Array + Blob: ", arrayResult);
 }
 
-function main() {
+async function main() {
     console.log("Serialization:");
-    runSerializationTests()
+    await runSerializationTests()
 }
 
 main();
