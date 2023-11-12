d8595739b157d34d88d69c52eb8f9004dc5f58b4
Konstantin Konstantin <zetslief@protonmail.com>
2023-11-12 14:34:03 +0100

    Lobby browser: a little bit of styling.

--- ui/dist/js/lobby.browser.guest.js
+++ ui/dist/js/lobby.browser.guest.js
@@ -1,16 +1,20 @@
 const BASE = new URL("http://localhost:5000/");
 
 const lobbies = document.getElementById("lobbies");
-const lobbyItemTemplate = document.getElementById("lobbyItemTemplate");
 
 window.onload = async () => {
+    const templates = (() => ({
+        default: document.getElementById("lobbyItemTemplate"),
+        requested: document.getElementById("requestedLobbyItemTemplate"),
+        disabled: document.getElementById("disabledLobbyItemTemplate"),
+    }))();
     const data = await requestLobbies(location);
     console.log(data);
-    renderLobbyData(data, template, storage);
+    renderLobbyData(data, templates, lobbies);
 
     setInterval(async () => {
         const data = await requestLobbies(location);
-        renderLobbyData(data, template, storage);
+        renderLobbyData(data, templates, lobbies);
     }, 1000)
 };
 
@@ -20,15 +24,28 @@ async function requestLobbies(baseUrl) {
     return response.json();
 } 
 
-function renderLobbyData(data, template, storage) {
+function renderLobbyData(data, templates, storage) {
+    function getChildElementByClassName(element, className) {
+        const selected = element.querySelector(`.${className}`);
+        if (selected && !Array.isArray(selected)) {
+            return selected;
+        } else {
+            return undefined;
+        }
+    }
+
     while (storage.firstChild) {
         storage.removeChild(storage.firstChild);
     }
     for (const item of data) {
-        const itemElement = template.cloneNode(true);
+        const itemElement = templates.default.cloneNode(true);
         itemElement.removeAttribute("id");
-        const valueElement = itemElement.lastElementChild;
-        valueElement.textContent = item.toString();
+        const lobbyNameElement = getChildElementByClassName(itemElement, "lobbyName");
+        lobbyNameElement.textContent = item.name;
+        const gameName = getChildElementByClassName(itemElement, "gameName");
+        gameName.textContent = item.game.name;
+        const numberOfPlayers = getChildElementByClassName(itemElement, "numberOfPlayers");
+        numberOfPlayers.textContent = item.game.numberOfPlayers;
         storage.appendChild(itemElement);
     }
 }
