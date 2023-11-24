c5cf19b4f4111cac7e56cc084da363baa7e68e16
Konstantin Konstantin <zetslief@protonmail.com>
2023-11-25 00:01:09 +0100

    BE: send browser page when user transitions to the lobby browsing stage.

--- backend/server/Program.cs
+++ backend/server/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 var loginPagePath = Path.GetFullPath("./../../ui/dist/login.html");
 var lobbyHostPage = Path.GetFullPath("./../../ui/dist/lobby.host.html");
 var lobbyGuestPage = Path.GetFullPath("./../../ui/dist/lobby.guest.html");
+var lobbyBrowserPage = Path.GetFullPath("./../../ui/dist/lobby.browser.html");
 var indexPagePath = Path.GetFullPath("./../../ui/dist/index.html");
 
 var games = new []
@@ -35,7 +36,7 @@ app.UseStaticFiles(new StaticFileOptions
 app.MapGet("/", GetMainPage);
 app.MapGet("/lobbies/{playerName}", GetLobbyInformation);
 app.MapGet("/lobbies/{lobbyId}/{playerName}/view", GetLobbyPage).WithName("get-host-page");
-app.MapGet("/lobbies/{playerName}/view", GetGuestPage).WithName("get-guest-page");
+app.MapGet("/lobbies/{playerName}/view", GetLobbyBrowserPage).WithName("get-guest-page");
 app.MapGet("/lobbies/{lobbyId}/{playerName}", GetLobbyData);
 app.MapPut("/lobbies/{lobbyId}/{playerName}", RenameLobby);
 app.MapPost("/lobbies", CreateLobby);
@@ -72,8 +73,8 @@ IResult GetLobbyPage(int lobbyId, string playerName)
             ?  Results.Content(File.ReadAllText(lobbyHostPage), "text/html")
             :  Results.Content(File.ReadAllText(lobbyGuestPage), "text/html");
 
-IResult GetGuestPage(string playerName)
-    => Results.Content(File.ReadAllText(lobbyGuestPage), "text/html");
+IResult GetLobbyBrowserPage(string playerName)
+    => Results.Content(File.ReadAllText(lobbyBrowserPage), "text/html");
 
 IResult GetLobbyData(int lobbyId, string playerName)
     => lobby is null 
