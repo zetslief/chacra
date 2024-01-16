afbef9da5668c0305afb9f1e14a9fff11461d85e
Konstantin Konstantin <zetslief@protonmail.com>
2024-01-16 20:34:10 +0100

    Add chat message default infrastucture.

--- ui/dist/js/lobby.guest.js
+++ ui/dist/js/lobby.guest.js
@@ -3,16 +3,36 @@ window.onload = main;
 const PLAYERS = "players";
 const PLAYER_TEMPLATE = "playerTemplate";
 
+const CHAT = "chat";
+const MESSAGE_TEMPLATE = "messageTemplate";
+
 async function main() {
+    const playersUpdate = createPlayerUpdate();
+    const chatUpdate = createChatUpdate();
+    const mainUpdate = async () => {
+        const lobbyData = await getLobbyData(); 
+        await playersUpdate(lobbyData);
+        await chatUpdate(lobbyData);
+    };
+    await mainUpdate();
+    setInterval(mainUpdate, 1000);
+}
+
+function createPlayerUpdate() {
     const playerTemplate = document.getElementById(PLAYER_TEMPLATE);
     const playersElement = document.getElementById(PLAYERS);
-    console.log("Hello, Guest page!.");
-    const update = async () => {
-        const lobbyData = await getLobbyData();
+    return async (lobbyData) => {
         renderPlayers(playersElement, lobbyData.players, playerTemplate);
     };
-    await update();
-    setInterval(update, 1000);
+}
+
+function createChatUpdate() {
+    const messageTemplate = document.getElementById(MESSAGE_TEMPLATE);
+    const chat = document.getElementById(CHAT);
+    return async (lobbyData) => {
+        console.log("Chat Update", chat, messageTemplate);
+        console.log("Chat data:", lobbyData);
+    };
 }
 
 async function getLobbyData()
