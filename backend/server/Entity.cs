9193324bf72ad0193cef26999b3ff4eebe19c658
Konstantin Konstantin <zetslief@protonmail.com>
2024-02-24 16:22:29 +0100

    Initial code to generate boosters on BE side.

--- backend/server/Entity.cs
+++ backend/server/Entity.cs
@@ -32,36 +32,73 @@ public class Entity : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (started)
-            {
-                started = false;
-                sending = true;
-                stopwatch.Start();
-                Console.WriteLine("Staring the game...");
-            }
-            if (finished)
-            {
-                stopwatch.Stop();
-                sending = false;
-                finished = false;
-                Console.WriteLine("Finishing the game...");
-            }
-            if (!sending)
-            {
-                stopwatch.Stop();
-                await Task.Delay(delayMs, stoppingToken).ConfigureAwait(false);
-                continue;
-            }
-            var elapsed = stopwatch.ElapsedMilliseconds;
-            if (elapsed < delayMs)
+            bool sent = ProcessDeltaTime(
+                stopwatch,
+                delayMs,
+                send,
+                ref started,
+                ref finished,
+                ref sending);
+            if (sending && !sent)
             {
                 await Task.Yield();
-                continue;
             }
-            stopwatch.Restart();
-            send(new DeltaState(elapsed));
+            else
+            {
+                await Task.Delay(delayMs).ConfigureAwait(false);
+            }
         }
     }
+
+    private static bool ProcessDeltaTime(
+        Stopwatch stopwatch,
+        int delay,
+        Action<DeltaState> send,
+        ref bool started, 
+        ref bool finished,
+        ref bool sending)
+    {
+        if (started)
+        {
+            started = false;
+            sending = true;
+            stopwatch.Start();
+            Console.WriteLine("Staring the game...");
+        }
+        if (finished)
+        {
+            stopwatch.Stop();
+            sending = false;
+            finished = false;
+            Console.WriteLine("Finishing the game...");
+        }
+        if (!sending)
+        {
+            stopwatch.Stop();
+            return false;
+        }
+        var elapsed = stopwatch.ElapsedMilliseconds;
+        if (elapsed < delay) return false;
+        stopwatch.Restart();
+        send(new DeltaState(elapsed));
+        return true;
+    }
+
+    private static bool ProcessBoosterSpawner(
+        Stopwatch stopwatch,
+        int boosterSpawnDelay,
+        Action<State> send)
+    {
+        static KnownBoosterState GenerateBooster()
+            => new KnownBoosterState("BiggerPlayer", "purple", 40);
+
+        var elapsed = stopwatch.ElapsedMilliseconds;
+        if (elapsed < boosterSpawnDelay) return false;
+        var booster = GenerateBooster();
+        send(booster);
+        return true;
+    }
+
 }
 
 public class EntityWriter
