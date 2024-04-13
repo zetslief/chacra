f9c73b1498be2abd1e28aad2c9e83dc2b60b5e60
Konstantin Konstantin <zetslief@protonmail.com>
2024-04-13 20:30:36 +0200

    Generate boosters based on chances/probability.

--- backend/server/BoosterFactory.cs
+++ backend/server/BoosterFactory.cs
@@ -10,11 +10,21 @@ public static class BoosterFactory
     public static BoosterDescription BiggerBall { get; } = new("biggerBall", "lightgreen");
     public static BoosterDescription DeathBall { get; } = new("deathBall", "darkred");
 
-    public static BoosterDescription[] All { get; } = [
-        BiggerPlayer,
-        BiggerBall,
-    ];
+    public const int BiggerPlayerChance = 40;
+    public const int BiggerBallChance = 90;
+    public const int DeathBallChance = 100;
+
+    public static BoosterDescription[] All { get; } = GenerateBoosterDescriptions();
 
     public static BoosterState Create(int index, BoosterDescription description)
         => new(index, description.Name, description.Color);
+
+    private static BoosterDescription[] GenerateBoosterDescriptions()
+        => Enumerable.Range(0, 100).Select(index => index switch
+        {
+            < BiggerPlayerChance => BiggerPlayer,
+            < BiggerBallChance => BiggerBall,
+            < DeathBallChance => DeathBall,
+            _ => throw new NotSupportedException("Chances greater than 100 does not exist :)"),
+        }).ToArray();
 }
